ab973800ee9f17ddec8a72fd58fe597a2dd4bf54
zawb-zhangzhichao <wb-zhangzhichao@zhongan.io>
2018-08-21 18:09:38 +0800

    写入通用验证消息

--- service/mock/api/formModel/addUser.js
+++ service/mock/api/formModel/addUser.js
@@ -8,34 +8,342 @@ const wrapper = function (value) {
 
 const formModels = {
 	templates: {
+		// 相等
 		'Equals': `
-					let ret = false
-          switch ($$.type()) {
-              case 'number': 
-                  ret = parseInt($$(0)) === parseInt($$(1))
-                  break
-              case 'date':
-                  let d1 = new Date($$(0))
-									let d2 = new Date($$(1))
-                  ret = d1.getTime() === d2.getTime()
-                  break
-              case 'string':
-                  ret = $$(0) === $$(1)
-                  break
-              case 'address':
-                  ret = false
-                  break;
-          }
-          return ret ? $$.pass() : $$.fail(0, '必须相等于')
-      `,
-
+			let ret = false
+			switch ($$.type()) {
+					case 'number': 
+							ret = parseInt($$(0)) === parseInt($$(1))
+							break
+					case 'date':
+							let d1 = new Date($$(0))
+							let d2 = new Date($$(1))
+							ret = d1.getTime() === d2.getTime()
+							break
+					case 'string':
+							ret = $$(0) === $$(1)
+							break
+					case 'address':
+							ret = false
+							break;
+			}
+			return ret ? $$.pass() : $$.fail(0, '必须相等')
+		`,
+		// 等于(不分大小写)
+		'EqualsIgnoreCase': `
+			let ret = false
+			switch ($$.type()) {
+					case 'number': 
+							ret = false
+							break
+					case 'date':
+							ret = false
+							break
+					case 'string':
+							ret = $$(0).toUpperCase() == $$(1).toUpperCase()
+							break
+					case 'address':
+							ret = false
+							break;
+			}
+			return ret ? $$.pass() : $$.fail(0, '必须相等')
+		`,
+		// 不等于
+		'NotEquals': `
+			let ret = false
+			switch ($$.type()) {
+					case 'number': 
+							ret = parseInt($$(0)) !== parseInt($$(1))
+							break
+					case 'date':
+							let d1 = new Date($$(0))
+							let d2 = new Date($$(1))
+							ret = d1.getTime() !== d2.getTime()
+							break
+					case 'string':
+							ret = $$(0) !== $$(1)
+							break
+					case 'address':
+							ret = false
+							break;
+			}
+			return ret ? $$.pass() : $$.fail(0, '必须不等于')
+		`,
+		// 不等于(不分大小写)
+		'NotEqualsIgnoreCase': `
+			let ret = false
+			switch ($$.type()) {
+					case 'number': 
+							ret = false
+							break
+					case 'date':
+							ret = false
+							break
+					case 'string':
+							ret = $$(0).toUpperCase() === $$(1).toUpperCase()
+							break
+					case 'address':
+							ret = false
+							break;
+			}
+			return ret ? $$.pass() : $$.fail(0, '必须不等于')
+		`,
+		// 小于
+		'LessThen': `
+			let ret = false
+			switch ($$.type()) {
+					case 'number': 
+							ret = parseInt($$(0)) < parseInt($$(1))
+							break
+					case 'date':
+							let d1 = new Date($$(0))
+							let d2 = new Date($$(1))
+							ret = d1.getTime() < d2.getTime()
+							break
+					case 'string':
+							ret = false
+							break
+					case 'address':
+							ret = false
+							break;
+			}
+			return ret ? $$.pass() : $$.fail(0, '必须小于')
+		`,
+		// 小于等于
+		'LessThenEquals': `
+			let ret = false
+			switch ($$.type()) {
+					case 'number': 
+							ret = parseInt($$(0)) <= parseInt($$(1))
+							break
+					case 'date':
+							let d1 = new Date($$(0))
+							let d2 = new Date($$(1))
+							ret = d1.getTime() <= d2.getTime()
+							break
+					case 'string':
+							ret = false
+							break
+					case 'address':
+							ret = false
+							break;
+			}
+			return ret ? $$.pass() : $$.fail(0, '必须小于等于')
+		`,
+		// 大于
 		'GreaterThan': `
-          if ($$(0).equals($$(1)) {
-              return $$.pass();
-          } else {
-              return $$.fail(0, '必须大于')
-          }
-      `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+								ret = parseInt($$(0)) > parseInt($$(1))
+								break
+						case 'date':
+								let d1 = new Date($$(0))
+								let d2 = new Date($$(1))
+								ret = d1.getTime() > d2.getTime()
+								break
+						case 'string':
+								ret = false
+								break
+						case 'address':
+								ret = false
+								break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须大于')
+		`,
+		// 大于等于
+		'GreaterThenEquals': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+								ret = parseInt($$(0)) >= parseInt($$(1))
+								break
+						case 'date':
+								let d1 = new Date($$(0))
+								let d2 = new Date($$(1))
+								ret = d1.getTime() >= d2.getTime()
+								break
+						case 'string':
+								ret = false
+								break
+						case 'address':
+								ret = false
+								break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须大于等于')
+		`,
+		// 在集合中
+		'In': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+								ret = false
+								break
+						case 'date':
+								let d1 = new Date($$(0))
+								let d2 = new Date($$(1))
+								ret = false
+								break
+						case 'string':
+								ret = false
+								break
+						case 'address':
+								ret = false
+								break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须在集合中')
+		`,
+		// 不在集合中
+		'NotIn': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+								ret = false
+								break
+						case 'date':
+								let d1 = new Date($$(0))
+								let d2 = new Date($$(1))
+								ret = false
+								break
+						case 'string':
+								ret = false
+								break
+						case 'address':
+								ret = false
+								break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须不在集合中')
+		`,
+		// 开始于
+		'StartWith': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+							let str1 = $$(0) + ''
+							let str2 = $$(1) + ''
+							ret = str1.startsWith(str2)
+							break
+						case 'date':
+								let d1 = new Date($$(0))
+								let d2 = new Date($$(1))
+								ret = false
+								break
+						case 'string':
+								ret = $$(0).startsWith($$(1))
+								break
+						case 'address':
+								ret = false
+								break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须开始于')
+		`,
+		// 不开始于
+		'NotStartWith': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+								let str1 = $$(0) + ''
+								let str2 = $$(1) + ''
+								ret = !str1.startsWith(str2)
+								break
+						case 'date':
+								let d1 = new Date($$(0))
+								let d2 = new Date($$(1))
+								ret = false
+								break
+						case 'string':
+							ret = !$$(0).startsWith($$(1))
+							break
+						case 'address':
+								ret = false
+								break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须不开始于')
+		`,
+		// 结束于
+		'EndWith': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+							let str1 = $$(0) + ''
+							let str2 = $$(1) + ''
+							ret = str1.endsWith(str2)
+							break
+						case 'date':
+							let d1 = new Date($$(0))
+							let d2 = new Date($$(1))
+							ret = false
+							break
+						case 'string':
+							ret = $$(0).endsWith($$(1))
+							break
+						case 'address':
+							ret = false
+							break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须结束于')
+		`,
+		// 不结束于
+		'NotEndWith': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+							let str1 = $$(0) + ''
+							let str2 = $$(1) + ''
+							ret = !str1.endsWith(str2)
+							break
+						case 'date':
+							let d1 = new Date($$(0))
+							let d2 = new Date($$(1))
+							ret = false
+							break
+						case 'string':
+							ret = !$$(0).endsWith($$(1))
+							break
+						case 'address':
+							ret = false
+							break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须不结束于')
+		`,
+		// 为空
+		'Null': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+							ret = $$(1) === ''
+							break
+						case 'date':
+							ret = $$(1) === ''
+							break
+						case 'string':
+							ret = $$(1) === ''
+							break
+						case 'address':
+							ret = false
+							break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须为空')
+		`,
+		// 不为空
+		'Null': `
+			let ret = false
+				switch ($$.type()) {
+						case 'number': 
+							ret = $$(1) !== ''
+							break
+						case 'date':
+							ret = $$(1) !== ''
+							break
+						case 'string':
+							ret = $$(1) !== ''
+							break
+						case 'address':
+							ret = false
+							break;
+				}
+			return ret ? $$.pass() : $$.fail(0, '必须不为空')
+		`,
 	},
 	formModels: {
 		p1: {
